527c68b5d7497adb2aeaaba70ce9c00d0139e1b0
misterGas <79254801+misterGas@users.noreply.github.com>
2025-02-21 12:19:34 +0100

    Delete Sito/Sito Gastronomia directory

--- Sito/Sito Gastronomia/connessione.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $connessione = new mysqli("localhost", "root", "", "GASTRONOMIA");
-  if ($connessione -> connect_errno) {
-    echo "Impossibile connettersi al server: " . $connessione -> connect_error;
-  }
-?>
--- Sito/Sito Gastronomia/gestisciUtenti.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once("connessione.php");
-    echo "<h1> Gestione Utenti </h1>";
-    $utenti = $connessione -> query("select * from UTENTE where TipoUtente = 'U'");
-    while ($riga = $utenti -> fetch_assoc()) {
-    	echo $riga["NomeUtente"] . " " . $riga["CognomeUtente"] . ", " . $riga["EmailUtente"] . "<br/>";
-    }
-?>
